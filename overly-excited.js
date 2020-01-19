17ec1534516f1c74e5e4cf4e46bc7145403b838f
Chase Sullins <jcsullins222@yahoo.com>
2020-01-19 10:45:36 -0600

    completed the Stacking exercise

--- overly-excited.js
+++ overly-excited.js
@@ -19,15 +19,24 @@ let sentence = [
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray) {
+function addExcitement(theWordArray, addChar) {
   // Each time the for loop executes, you're going to add one more word to this string
   let buildMeUp = "";
-
+  console.log(addChar)
   for (let i = 0; i < theWordArray.length; i++) {
+   
     // Concatenate the new word onto buildMeUp
     // Print buildMeUp to the console
+    currentWord = i + 1
+    if (currentWord % 3 === 0){
+    buildMeUp = buildMeUp.concat(sentence[i], addChar)
+    }
+    else {
+      buildMeUp = buildMeUp.concat(sentence[i], " ")
+    }
+    console.log(buildMeUp)
   }
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence);
+addExcitement(sentence, "? " );
